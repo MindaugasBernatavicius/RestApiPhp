4efaa6aeca07af90aa0218963c956591c674a2e7
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2020-05-11 09:58:04 +0300

    Adding some comments

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         public function setName($name){ $this->name = $name;}
         public function getSurname(){ return $this->surname; }
         public function setSurname($surname){ $this->surname = $surname;}
+
         public function jsonSerialize() {
             $vars = get_object_vars($this);
             return $vars;
@@ -23,10 +24,10 @@
 
     // Read JSON file
     $FILE = 'authors.json';
-    $json_file = file_get_contents($FILE);
-    $authors_arr = json_decode($json_file, true);
+    $json_string = file_get_contents($FILE);
+    $authors_arr = json_decode($json_string, true);
 
-    // create authors from file
+    // create authors from file (deserializacija)
     $authors = [];
     foreach($authors_arr as $author)
         array_push($authors, new Author(
@@ -38,20 +39,32 @@
     // Logika
     switch($_SERVER['REQUEST_METHOD']){
         case 'POST': 
+            // ... create new author
+            // ... $ curl http://localhost/RestApiPhp/authors -s -D - -X POST -d '{"id":"5", "na"surname":"B."}'
+            // ...  HTTP/1.1 200 OK
+            // ...  Date: Mon, 11 May 2020 06:52:30 GMT
+            // ...  Server: Apache/2.4.41 (Win64) OpenSSL/1.1.1c PHP/7.3.11
+            // ...  X-Powered-By: PHP/7.3.11
+            // ...  Content-Length: 0
+            // ...  Content-Type: text/html; charset=UTF-8
             if($_SERVER['REQUEST_URI'] === "/RestApiPhp/authors"){
                 $inputJSON = file_get_contents('php://input');
-                $x = json_decode($inputJSON, true);
-                array_push($authors, new Author($x['id'], $x['name'],$x['surname']));
-                $people_arr = json_encode($authors);
-                file_put_contents($FILE, $people_arr);
+                $author_string = json_decode($inputJSON, true);
+                array_push($authors, new Author($author_string['id'], $author_string['name'],$author_string['surname']));
+                $author_arr = json_encode($authors);
+                file_put_contents($FILE, $author_arr);
             }
             break;
         case 'GET': 
             // ... get all authors
+            // ... $ curl http://localhost/RestApiPhp/authors -s -X GET
+            // ... [{"id":"1","name":"Marytė","surname":"Melnikaitė"},{"id":"2","name":"Ignius","surname":"Knyguolis"},{"id":"3","name":"Jonas","surname":"Biliunas"}]
             if($_SERVER['REQUEST_URI'] === "/RestApiPhp/authors")
                 prnt($authors);
 
             // ... get user by id
+            // ... $ curl http://localhost/RestApiPhp/authors/1 -s -X GET
+            // ... {"id":"1","name":"Marytė","surname":"Melnikaitė"}
             elseif (substr($_SERVER['REQUEST_URI'], 0, strlen("/RestApiPhp/authors/")) === "/RestApiPhp/authors/"){
                 $id = end(explode('/', $_SERVER['REQUEST_URI']));
                 foreach($authors as $author) 
